67eed62242b9aa3336b769cbbdac48675840cd78
arka <arkashankarpradhan@gmail.com>
2022-04-19 13:10:29 +0530

    firstcommit

---
+++ src/main/java/com/example/demojpa/service/EmpService.java
@@ -0,0 +1,32 @@
+package com.example.demojpa.service;
+
+import com.example.demojpa.entity.Emp;
+import com.example.demojpa.repository.EmpRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Service
+public class EmpService {
+    @Autowired
+    EmpRepository empRepository;
+    public Boolean add(Emp emp) {
+        empRepository.save(emp);
+        return true;
+    }
+
+    public Iterable<Emp> getall(){
+        return empRepository.findAll();
+    }
+
+    public List<Emp> getEmpByName(String name){
+        return empRepository.findEmpByName("%" + name + "%");
+    }
+
+    public List<Emp> getSalaryGreaterThan(Double salary){
+        return empRepository.findSalaryGreater(salary);
+    }
+
+}
